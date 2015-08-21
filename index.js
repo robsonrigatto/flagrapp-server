2016094c1a5e521c4a9642987e1d4ee95ec78958
Robson Rigatto <rigatto.robson@gmail.com>
2015-08-20 21:21:19 -0300

    Mudando credenciais

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(bodyParser.json());
  
 //Vamos criar um twitter senão cada postagem vai no meu nome
 var client = new Twitter({
-    "consumer_key": "AyjONhycsQBqImpcmkcFDlvL8",
-    "consumer_secret": "o9TXHNFvLEFhzMLxMtW4QqWgIQhNxpHgeuOlySHqC6PjSkFaRw",
+    "consumer_key": "olqq23s4wCDJB4gqQzBrIP9We",
+    "consumer_secret": "P4lipmSK1JuvZqo43RzMnjVH0KcrjL57ZfrTgm7H2VNqC0pdyP",
     "access_token_key": "3430114486-WjMHKawuKgSzh12EtR7VXUVJ1x91AnbEVagCkdG",
     "access_token_secret": "3Xv8WZ5UUo04zcC6HUvBdtKMiliLWtBHDvnurgJFsaQm0"
 });
@@ -19,7 +19,7 @@ var router = express.Router();
 router.get('/timeline', function(req, res) {
 	//screen_name=foraPT
 	var params = req.query
-	client.get('statuses/user_timeline.json', params, function(error, tweets, response){
+	client.get('statuses/user_timeline', params, function(error, tweets, response){
 	  if (error) {
 		res.json({type:'error', message:error});
 	  } else {
